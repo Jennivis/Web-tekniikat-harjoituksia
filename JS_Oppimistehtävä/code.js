5a5e0eac22b2108e40a44c2fa405f0e12cfad87b
Jennivis <n1vije00@students.oamk.fi>
2021-12-28 23:12:50 +0200

    Lisätty selektoreita ~ ja >

--- JS_Oppimistehtävä/code.js
+++ JS_Oppimistehtävä/code.js
@@ -1,31 +1,50 @@
-let q = ""
-let empty = "Syötä kaupunki."
-let appID = "6f2ca9083c4737d305a583ac6b71d6eb"
+let city = "";
+let units = "";
+let empty = "Syötä kaupunki.";
+let degrees = "";
+let condition = "";
+let rain = "";
 
-function get_weather () {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather", true);
-}
+function get_weather(){
+    
+    city = document.getElementById("city").value;
+    let url = "http://api.openweathermap.org/data/2.5/find?q=" + city + "&units=metric&appid=6f2ca9083c4737d305a583ac6b71d6eb";
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange=function() {
+        if(this.readyState === 4 && this.status === 200){
+        let weather = JSON.parse(this.response);
+        document.getElementById("degrees").innerHTML = weather.list[0].main.temp;
+        degrees = weather.list[0].main.temp;
+        condition = weather.list[0].weather.id;
+        }
+    };
+
+    xhttp.open("GET", url, true);
+
+    xhttp.send();
+}    
+
 
 function searchWeather () {
-    q = document.getElementById("city").value;
-
-    if (q =! "") {
-        get_weather (q)
+    
+    if (city =! "") {
+        get_weather ()
     } else {
         empty
         document.getElementById("city").focus();
     }
+    classification();
 }
 
-function classification() {
-    if (sää >= 20 && weather.id == 800) {
-        document.getElementById("whatToDo").innerHTML = "Keli on lämmin ja aurinkoinen. Olisiko rantapäivä tänään?"
-    } else if (sää > 5 && sää < 20 && rain == rain.h) {
-        document.getElementById("whatToDo").innerHTML = "Sadetta luvassa. Muista sateenvarjo ja kumisaappaat!"
-    } else if (sää > -5 && sää < 5) {
-        document.getElementById("whatToDo").innerHTML = "Koleaa säätä luvassa. Muista pukeutua lämpimämmin."
-    } else if (sää < -5 && weather.id == 800) {
-        document.getElementById("whatToDo").innerHTML = "Mainio ulkoilukeli tänään! Pitäisikö mennä luistelemaan tai hiihtämään?"
-    }
+ function classification() {
+     if (degrees >= 20 && condition == 800) {
+         document.getElementById("whatToDo").innerHTML = "Keli on lämmin ja aurinkoinen. Olisiko rantapäivä tänään?"
+     } else if (degrees > 5 && degrees < 20) {
+         document.getElementById("whatToDo").innerHTML = "Ei mikään rantakeli tänään ainakaan. Ulkoilla voisit kuitenkin."
+     } else if (degrees > -5 && degrees < 5) {
+         document.getElementById("whatToDo").innerHTML = "Kolea sää tänään. Muista pukeutua lämpimämmin."
+     } else if (degrees < -5 && condition == 800) {
+         document.getElementById("whatToDo").innerHTML = "Mainio ulkoilukeli tänään! Pitäisikö mennä luistelemaan tai hiihtämään?"
+     }
 }
